e7ecc70bf1fc92081e1dd7a4f68350eaedc839f5
TW <tomasz.wojdynski@icloud.com>
2022-11-20 12:36:15 +0100

    Student constructor

--- src/main/java/pl/sdacademy/java/patterns/Student.java
+++ src/main/java/pl/sdacademy/java/patterns/Student.java
@@ -5,6 +5,10 @@ class Student implements HasPerson {
     private Person person;
     private String groupCode;
 
+    public Student (Person person, String groupCode){
+        this.person = person;
+        this.groupCode = groupCode;
+    }
 
     @Override
     public Person getPerson() {
